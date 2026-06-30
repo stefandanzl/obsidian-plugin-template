adadeeba245cd06dcf6aec65cf3701e5f0179692
Stefan Danzl <stefan.danzl@live.de>
2026-06-30 14:38:55 +0200

    updated default src folder files

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import { Plugin } from 'obsidian';
-import { MySettings, DEFAULT_SETTINGS, MyPluginSettingTab } from './ui/settings';
-import { registerCommands } from './commands';
+import { Plugin } from "obsidian";
+import { DEFAULT_SETTINGS } from "./settings";
+import { MySettings } from "./settings";
+import MyPluginSettingTab from "./settings";
 
 export default class MyPlugin extends Plugin {
-	settings: MySettings;
+	declare settings: MySettings;
 
 	async onload() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 
 		// Add settings tab
 		this.addSettingTab(new MyPluginSettingTab(this.app, this));
-
-		// Register commands
-		registerCommands(this);
 	}
 }
